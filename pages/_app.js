27699a2af691f6d5f7795992f1673580f7843340
Xavier Collantes <collantes.xavier@gmail.com>
2022-10-19 21:10:46 -0700

    Add home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
+import Head from 'next/head'
+import { Container, ThemeProvider } from '@mui/material'
+import TopBar from '../components/topbar'
 import '../styles/globals.css'
 
+import theme from '../themes/theme'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>X</title>
+        <meta name="description"
+          content="Technical portfolio, blog, and resume for Xavier Collantes." />
+        <meta content="text/html; charset=utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <TopBar></TopBar>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  )
 }
